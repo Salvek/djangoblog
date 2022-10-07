0f7a5241d13f02306de2328a3606833d5b07ef95
Sławomir Śpiewak <salvek.mtb@gmail.com>
2022-10-07 13:32:59 +0200

    Dodanie zmian w pliku html

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from .models import Post
 
 def post_list(request):
-    posts = Post.objects.filter(publishedDate__lte=timezone.now()).order_by('publishedDate')
+    posts = Post.objects.filter(publishDate__lte=timezone.now()).order_by('publishedDate')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
